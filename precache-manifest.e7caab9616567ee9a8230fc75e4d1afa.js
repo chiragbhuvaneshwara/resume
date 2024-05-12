31bd0dedf141aec933512163283ca5c0df4c1622
Chirag <chiragbhuvaneshwar@gmail.com>
2024-05-12 03:04:02 +0200

    Updates

---
+++ precache-manifest.e7caab9616567ee9a8230fc75e4d1afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e3aade5dcd844874f6f3ca6da828af",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "120c77dab07a4d677418",
+    "url": "/resume/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "5c1bdde6624d6db9e31a",
+    "url": "/resume/static/js/2.8f37d8d5.chunk.js"
+  },
+  {
+    "revision": "e65922e1a95aafdda6f547e02a94b171",
+    "url": "/resume/static/js/2.8f37d8d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9266460776d6265b855",
+    "url": "/resume/static/js/3.fdfc3da4.chunk.js"
+  },
+  {
+    "revision": "120c77dab07a4d677418",
+    "url": "/resume/static/js/main.e54d97b7.chunk.js"
+  },
+  {
+    "revision": "b0c7344e6b5b6116ef60",
+    "url": "/resume/static/js/runtime-main.eda79685.js"
+  }
+]);
